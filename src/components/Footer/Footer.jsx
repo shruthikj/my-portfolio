1f0375d579edd529053e329092d0d41c5e0dfef7
Shruthi Krithika Jyothi Sunder <shruthikrithika.j@gmail.com>
2021-06-22 07:30:54 -0500

    "About text changes, gradient"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
               return (
                 <a
                   key={id}
-                  href={url || 'https://github.com/cobidev/gatsby-simplefolio'}
+                  href={url || ''}
                   rel="noopener noreferrer"
                   target="_blank"
                   aria-label={name}
@@ -38,13 +38,13 @@ const Footer = () => {
         </div>
         <hr />
         <p className="footer__text">
-          © {new Date().getFullYear()} - Template developed by{' '}
-          <a href="https://github.com/cobidev" target="_blank" rel="noopener noreferrer">
-            Jacobo Martínez
+          © {new Date().getFullYear()} - Developed by{' '}
+          <a href="" target="_blank" rel="noopener noreferrer">
+            Shruthi Krithika
           </a>
         </p>
 
-        {isEnabled && <GithubButtons />}
+        {/* {isEnabled && <GithubButtons />} */}
       </Container>
     </footer>
   );
--- src/mock/data.js
+++ src/mock/data.js
@@ -2,25 +2,25 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
+  title: 'Shruthi Krithika - Portfolio', // e.g: 'Name | Developer'
   lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Shruthi Krithika',
+  subtitle: "I'm a Full Stack Software Developer",
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne: 'I am Shruthi. I have 4+ years of professional experience as a Full Stack Software Engineer. I also hold a Masters in Computer Science from UTA. I recently interned at HealCo, a telemedicine startup, as a Full Stack Engineer. Prior to that, I worked at ZOHO building end-to-end modules for b2C, b2b and enterprise consumption on cloud. I am experienced in building Low-Code Platform applications.',
+  paragraphTwo: 'My strong tech stack would be: Java, Python, JavaScript, React.js, TypeScript, HTML, CSS, MySql.',
+  paragraphThree: 'Apart from my professional side, if I must describe myself, I am a thriller/horror movies aficionado. I love dancing, sketching, DIY projects and yoga. Off late I have gotten into a lot of baking because of the pandemic and the lockdown. :)',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -33,7 +33,7 @@ export const projectsData = [
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -42,7 +42,7 @@ export const projectsData = [
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -51,7 +51,7 @@ export const projectsData = [
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
